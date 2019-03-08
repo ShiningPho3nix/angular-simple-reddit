a9ac286d188178ef0f9996b471fe17b33cf3c80e
Steffen Dworsky <steffen.dworsky@gmx.de>
2019-03-08 08:44:43 +0100

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+      new Article('Steffens großartiger Plan!', 'http://schmuddelseite.xxx', 69),
+      new Article('Ramón hatte eine Idee UwU', 'http://russiangayporn.org', 420),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    title.value='';
+    link.value='';
+    return false;
+  }
+}
